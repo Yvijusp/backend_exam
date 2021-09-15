e96fe592aeb58fdbe195b1ccb6b378389f040733
Yvijusp <yvijus@hotmail.com>
2021-09-15 18:03:51 +0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,107 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+const app = express();
+
+// middlwares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send({ message: 'Api is running' });
+});
+
+// Returning 10 jokes
+app.get('/api/jokes', async (req, res) => {
+  try {
+    let jokes = [];
+
+    for (let i = 0; i < 10; i++) {
+      const result = await fetch(
+        'https://api.chucknorris.io/jokes/random'
+      ).then((response) => response.json());
+
+      jokes.push(result.value);
+    }
+
+    res.send(jokes);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Returning jokes by assigned amount value
+app.get('/api/jokes/:amount', async (req, res) => {
+  try {
+    let jokes = [];
+
+    for (let i = 0; i < req.params.amount; i++) {
+      const result = await fetch(
+        'https://api.chucknorris.io/jokes/random'
+      ).then((response) => response.json());
+
+      jokes.push(result.value);
+    }
+
+    res.send(jokes);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Returning 10 jokes by assigning category
+app.get('/api/jokes/category/:category', async (req, res) => {
+  try {
+    let jokes = [];
+
+    for (let i = 0; i < 10; i++) {
+      const result = await fetch(
+        `https://api.chucknorris.io/jokes/random?category=${req.params.category}`
+      ).then((response) => response.json());
+
+      if (result.status === 404)
+        return res.status(404).send({
+          error: 'Not found',
+          message: `No jokes for category ${req.params.category} found`,
+        });
+
+      jokes.push(result.value);
+    }
+
+    res.send(jokes);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Returning jokes by category using a specified amount
+app.get('/api/jokes/category/:category/:amount', async (req, res) => {
+  try {
+    let jokes = [];
+
+    for (let i = 0; i < req.params.amount; i++) {
+      const result = await fetch(
+        `https://api.chucknorris.io/jokes/random?category=${req.params.category}`
+      ).then((response) => response.json());
+
+      if (result.status === 404)
+        return res.status(404).send({
+          error: 'Not found',
+          message: `No jokes for category ${req.params.category} found`,
+        });
+
+      jokes.push(result.value);
+    }
+
+    res.send(jokes);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(process.env.PORT, () => console.log('Api is running'));
---
+++ frontend/src/App.js
@@ -0,0 +1,151 @@
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`;
+
+const Wrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+
+  h1 {
+    text-align: center;
+  }
+
+  .container {
+    max-width: 600px;
+    background-color: #fdfdfd;
+
+    margin: 20px auto;
+    padding: 15px 10px;
+
+    box-shadow: 0 0 5px -1px rgba(0, 0, 0, 0.8);
+    border-radius: 15px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h2 {
+      margin: 0 0 15px 0;
+      text-align: center;
+    }
+
+    a {
+      border: #666 solid 1px;
+      padding: 5px 15px;
+      border-radius: 5px;
+      display: block;
+      width: fit-content;
+    }
+
+    p {
+      margin: 5px 0;
+    }
+
+    ul {
+      margin-top: 5px;
+      list-style: none;
+      column-count: 3;
+      column-gap: 25px;
+    }
+  }
+`;
+
+function App() {
+  const categories = [
+    'animal',
+    'career',
+    'celebrity',
+    'dev',
+    'explicit',
+    'fashion',
+    'food',
+    'history',
+    'money',
+    'movie',
+    'music',
+    'political',
+    'religion',
+    'science',
+    'sport',
+    'travel',
+  ];
+
+  const randomCategory =
+    categories[Math.floor(Math.random() * categories.length)];
+
+  const randomNumber = Math.floor(Math.random() * 20 + 1);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>Random Chuck Norris jokes API</h1>
+
+        <div className='container'>
+          <h2>10 random Chuck Norris jokes route</h2>
+          <a href='https://chucknorris-api.herokuapp.com/api/jokes'>
+            https://chucknorris-api.herokuapp.com/api/jokes
+          </a>
+          <p>Gets 10 random jokes from api</p>
+        </div>
+
+        <div className='container'>
+          <h2>Random Chuck Norris jokes by amount route</h2>
+          <a
+            href={`https://chucknorris-api.herokuapp.com/api/jokes/${randomNumber}`}
+          >
+            https://chucknorris-api.herokuapp.com/api/jokes/:amount
+          </a>
+          <p>Gets random amount of jokes by specified amount</p>
+          <p>This example will always get an array between 1 to 20 jokes</p>
+        </div>
+
+        <div className='container'>
+          <h2>10 random Chuck Norris jokes by category</h2>
+          <a
+            href={`https://chucknorris-api.herokuapp.com/api/jokes/category/${randomCategory}`}
+          >
+            https://chucknorris-api.herokuapp.com/api/jokes/category/:category
+          </a>
+          <p>Gets 10 random jokes from a specified category</p>
+          <p>Clicking on link here will send you to a random category</p>
+          <h4>Available categories:</h4>
+          <ul>
+            {categories.map((category) => (
+              <li key={Math.random()}>{category}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='container'>
+          <h2>Random Chuck Norris jokes by category with specified amount</h2>
+          <a
+            href={`https://chucknorris-api.herokuapp.com/api/jokes/category/${randomCategory}/${randomNumber}`}
+          >
+            https://chucknorris-api.herokuapp.com/api/jokes/category/:category/:amount
+          </a>
+          <p>Gets jokes from specified category and specified amount</p>
+          <p>
+            Clicking on link will select a random category and a random amount
+            between 1 and 20
+          </p>
+          <h4>Available categories</h4>
+          <ul>
+            {categories.map((category) => (
+              <li key={Math.random()}>{category}</li>
+            ))}
+          </ul>
+        </div>
+      </Wrapper>
+    </>
+  );
+}
+
+export default App;
